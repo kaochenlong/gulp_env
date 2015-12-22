d6e4dab489a78fedfc2f2701e22fad10451bc90e
Eddie Kao <eddie@digik.com.tw>
2015-12-22 19:37:03 +0800

    add build task

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,8 +1,19 @@
 'use strict';
 
-import gulp from 'gulp'
+import gulp       from 'gulp'
+import browserify from 'browserify';
+import babelify   from 'babelify';
+import source     from 'vinyl-source-stream';
 
 gulp.task('hello', () => {
   console.log('Hello, Gulp!');
+});
+
+gulp.task('build', () => {
+  return browserify('./source/app.js')
+  .transform(babelify)
+  .bundle()
+  .pipe(source('app.js'))
+  .pipe(gulp.dest('./build/'));
 });
 
